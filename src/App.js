00f975bd9c09b92de29a066641895aae75e5009a
DanielaPozo23 <garciapozoerikadaniela@gmail.com>
2025-06-10 11:18:03 -0600

    Agregue Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import AgendaVisita from "./pages/AgendaVisita";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Routes>
+          <Route path="/agenda" element={<AgendaVisita />} />
+          {/* Puedes agregar más rutas aquí */}
+          <Route
+            path="/"
+            element={
+              <header className="App-header">
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+              </header>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
